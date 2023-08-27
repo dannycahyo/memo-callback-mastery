7a1efe5ad09fafae87832667ceeebc72c26d298a
dannycahyo <dannydwicahyono@gmail.com>
2023-08-27 09:50:03 +0700

    feat: Implement useApiRequest custom hooks

---
+++ src/hooks/useApiRequest.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect, useCallback, useRef } from "react";
+
+interface UseApiRequestProps<T> {
+  apiFunction: () => Promise<T>;
+  initialData?: T;
+}
+
+interface UseApiResponse<T> {
+  data?: T;
+  isLoading: boolean;
+  error?: Error;
+  refetch: () => Promise<void>;
+}
+
+export const useApiRequest = <T>({
+  apiFunction,
+  initialData,
+}: UseApiRequestProps<T>): UseApiResponse<T> => {
+  const [data, setData] = useState<T | undefined>(initialData);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | undefined>();
+
+  const apiFunctionRef = useRef(apiFunction);
+
+  useEffect(() => {
+    apiFunctionRef.current = apiFunction;
+  }, [apiFunction]);
+
+  const fetchData = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await apiFunctionRef.current();
+      setData(response);
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { data, isLoading, error, refetch: fetchData };
+};
